b0f319651d7b3e99c41e90954fa229ee7ebeb6a1
Marcos Grillo <marcos.grillor@gmail.com>
2014-06-26 15:15:59 -0430

    - Added: Apache license

--- cpphop.cpp
+++ cpphop.cpp
@@ -1,3 +1,25 @@
+/*
+cpphop, version 1.0.0 -- a simple SHOP-like planner based in PyHop written in C++.
+Author: Marcos Grillo, 2014.06.26
+
+Copyright 2014 Marcos Grillo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+Cpphop should work correctly with g++ 4.8.2 and above, it needs the boost library
+
+For examples of how to use it, see the example files that come with Cpphop.
+*/
+
 #include <iostream>
 #include "cpphop.hpp"
 
